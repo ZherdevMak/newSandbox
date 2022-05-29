5730a3041c55ed060e5d770ad1b3d5b66d98050b
ZherdevMak <Zherdev.mak@gmail.com>
2022-05-29 15:46:18 +0300

    SimpleMusicEncoder_start

--- SimpleMusicEncoder.js
+++ SimpleMusicEncoder.js
@@ -2,11 +2,11 @@
 
 function compress(music) {
     for (let i = 0; i < music.length; i++) {
-         let nextI = i + 1;
-    let step = 2
-    if (music[i] === music[nextI]) {
-        let start = i;
-        one: for (let a = nextI; a<music.length; a++) {
+        let nextI = i + 1;
+        let step = 2
+        if (music[i] === music[nextI]) {
+            let start = i;
+            one: for (let a = nextI; a<music.length; a++) {
                 let finish = a;
                 if (music[a] !== music[a+1]) {
                     music.splice(start,finish,`,${music[start]}*${finish - start + 1}`);
@@ -15,7 +15,7 @@ function compress(music) {
             }
         }
     }
-return music
+    return music
 }
 
 alert(compress([1,2,2,2,2,3,4,4,4]))
