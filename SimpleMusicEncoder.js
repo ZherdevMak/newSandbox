37fb338f9daed8ded51c95bc85fd439f6ae3e986
ZherdevMak <Zherdev.mak@gmail.com>
2022-05-29 22:05:24 +0300

    SimpleMusicEncoder_step1

--- SimpleMusicEncoder.js
+++ SimpleMusicEncoder.js
@@ -1,21 +1,48 @@
 'use strict';
 
 function compress(music) {
+
     for (let i = 0; i < music.length; i++) {
         let nextI = i + 1;
-        let step = 2
-        if (music[i] === music[nextI]) {
+        if (music[i] + 1 === music[nextI] && music[nextI] + 1 === music[nextI+1]) {
             let start = i;
-            one: for (let a = nextI; a<music.length; a++) {
-                let finish = a;
-                if (music[a] !== music[a+1]) {
-                    music.splice(start,finish,`,${music[start]}*${finish - start + 1}`);
+            let finish = 3;
+            one: for (let a = nextI + 1; a<music.length; a++) {
+                if (music[a] + 1 !== music[a+1]) {
+                    music.splice(i,finish,`,${music[start]}-${music[a]}`);
                     break one
-                }
+                } else {++finish}
+            }
+        }
+    }
+    for (let i = 0; i < music.length; i++) {
+        let nextI = i + 1;
+        if (music[i] - 1 === music[nextI] && music[nextI] - 1 === music[nextI+1]) {
+            let start = i;
+            let finish = 3;
+            one: for (let a = nextI + 1; a<music.length; a++) {
+                if (music[a] - 1 !== music[a+1]) {
+                    music.splice(i,finish,`,${music[start]}-${music[a]}`);
+                    break one
+                } else {++finish}
+            }
+        }
+    }
+
+    for (let i = 0; i < music.length; i++) {
+        let nextI = i + 1;
+        if (music[i] === music[nextI]) {
+            let start = 0;
+            let finish = 2;
+            one: for (let a = nextI; a<music.length; a++) {
+                if (music[a] !== music[a+1]) {
+                    music.splice(i,finish,`,${music[i]}*${finish - start}`);
+                    break one
+                } else {++finish}
             }
         }
     }
     return music
 }
 
-alert(compress([1,2,2,2,2,3,4,4,4]))
+alert(compress([1,2,3,2,2,2,2,3,4,4,4]))
