867820c2e2d152d13f590aa1356f15c80cbd0ca1
ZherdevMak <Zherdev.mak@gmail.com>
2022-06-02 20:32:18 +0300

    Run-length A Simple Music Encoder

--- SimpleMusicEncoder.js
+++ SimpleMusicEncoder.js
@@ -1,48 +1,66 @@
 'use strict';
 
 function compress(music) {
-
     for (let i = 0; i < music.length; i++) {
         let nextI = i + 1;
-        if (music[i] + 1 === music[nextI] && music[nextI] + 1 === music[nextI+1]) {
+        let c = music[nextI] - music[i]
+        if (music[nextI] + c === music[nextI + 1] && (c > 1 || c < -1)) {
             let start = i;
             let finish = 3;
-            one: for (let a = nextI + 1; a<music.length; a++) {
-                if (music[a] + 1 !== music[a+1]) {
-                    music.splice(i,finish,`,${music[start]}-${music[a]}`);
+            one: for (let a = nextI + 1; a < music.length; a++) {
+                if (music[a] + c !== music[a + 1]) {
+                    if (c < 0) {
+                        c = c * -1
+                    }
+                    ;
+                    music.splice(i, finish, `${music[start]}-${music[a]}/${c}`);
                     break one
-                } else {++finish}
+                } else {
+                    ++finish
+                }
             }
         }
-    }
-    for (let i = 0; i < music.length; i++) {
-        let nextI = i + 1;
-        if (music[i] - 1 === music[nextI] && music[nextI] - 1 === music[nextI+1]) {
+
+        if (music[i] + 1 === music[nextI] && music[nextI] + 1 === music[nextI + 1]) {
             let start = i;
             let finish = 3;
-            one: for (let a = nextI + 1; a<music.length; a++) {
-                if (music[a] - 1 !== music[a+1]) {
-                    music.splice(i,finish,`,${music[start]}-${music[a]}`);
+            one: for (let a = nextI + 1; a < music.length; a++) {
+                if (music[a] + 1 !== music[a + 1]) {
+                    music.splice(i, finish, `${music[start]}-${music[a]}`);
                     break one
-                } else {++finish}
+                } else {
+                    ++finish
+                }
             }
         }
-    }
 
-    for (let i = 0; i < music.length; i++) {
-        let nextI = i + 1;
+        if (music[i] - 1 === music[nextI] && music[nextI] - 1 === music[nextI + 1]) {
+            let start = i;
+            let finish = 3;
+            one: for (let a = nextI + 1; a < music.length; a++) {
+                if (music[a] - 1 !== music[a + 1]) {
+                    music.splice(i, finish, `${music[start]}-${music[a]}`);
+                    break one
+                } else {
+                    ++finish
+                }
+            }
+        }
         if (music[i] === music[nextI]) {
             let start = 0;
             let finish = 2;
-            one: for (let a = nextI; a<music.length; a++) {
-                if (music[a] !== music[a+1]) {
-                    music.splice(i,finish,`,${music[i]}*${finish - start}`);
+            one: for (let a = nextI; a < music.length; a++) {
+                if (music[a] !== music[a + 1]) {
+                    music.splice(i, finish, `${music[i]}*${finish - start}`);
                     break one
-                } else {++finish}
+                } else {
+                    ++finish
+                }
             }
         }
     }
-    return music
+
+    return music.toString()
 }
 
-alert(compress([1,2,3,2,2,2,2,3,4,4,4]))
+alert(compress([1,2,2,3]))
